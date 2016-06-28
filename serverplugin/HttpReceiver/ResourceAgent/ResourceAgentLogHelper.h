33af5dd5ccad8c04b8d7606ade3418b54174b91b
zhaogang <zhaogang@shandagames.com>
2016-06-28 11:33:57 +0800

    remove httpreceiver to clientplugin

--- serverplugin/HttpReceiver/ResourceAgent/ResourceAgentLogHelper.h
+++
@@ -1,9 +0,0 @@
-#ifndef _RESOURCE_AGENT_LOG_HELP_H_
-#define _RESOURCE_AGENT_LOG_HELP_H_
-
-#include "AsyncVirtualClientLog.h"
-
-#define RA_XLOG HTTP_REC_XLOG
-#define RA_SLOG HTTP_REC_SLOG
-
-#endif //_RESOURCE_AGENT_LOG_HELP_H_
